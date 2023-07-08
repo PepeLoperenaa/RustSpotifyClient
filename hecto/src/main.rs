d1beb5b8224889f83c1c3b97028598939ce3ac9c
PepeLoperenaa <pepeloperena@gmail.com>
2023-07-08 08:57:49 +0100

    Update main.rs
    Added more functionality to main function and add crossterm to cargo.toml

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,8 +1,18 @@
-use std::io::{self, Read};
+use std::io::{self,Read, Write};
+
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result,
+    event,
+};
 
 fn main() {
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
         println!("{}", c);
+        if c == 'q' {
+            break;
+        }
     }
 }
